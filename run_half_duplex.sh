b18d46317a35fbd69fd88eead76b7e1d49db6672
Wagner M Cunha <wagnermc506@gmail.com>
2021-09-07 12:11:35 -0300

    Added script to run half-duplex client and server

---
+++ run_half_duplex.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+# Cada script deve ser executado em um terminal diferente.
+
+if [[ $1 == "" ]];
+then
+    echo "usage: $1 [client|server] <tamanho da dpu>"
+fi
+
+# Header + trailer da DPU tem 11 bytes, logo o tamanho mínimo são 12 para enviar alguma informação
+if [[ $1 == "client" && $2 > 11 ]];
+then
+    ./bin/client $2 &
+    ./bin/app client $!
+fi
+
+if [[ $1 == "server" && $2 > 11 ]];
+then
+    ./bin/server $2 &
+    ./bin/app server $!
+fi
