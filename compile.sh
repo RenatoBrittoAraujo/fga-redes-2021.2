950907d1409c0c0ec4676f3586faae3880d8715d
Wagner M Cunha <wagnermc506@gmail.com>
2021-09-07 21:58:00 -0300

    renaming files

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 echo "Compilando camada de enlace: cliente"
-gcc src/network_layer/bidiretional_node_a.c src/util/frame.c src/util/mq_utils.c -o bin/client -lrt
+gcc src/link_layer/bidiretional_node_a.c src/util/frame.c src/util/mq_utils.c -o bin/client -lrt
 
 echo "Compilando camada de enlace: server"
-gcc src/network_layer/bidiretional_node_b.c src/util/frame.c src/util/mq_utils.c -o bin/server -lrt
+gcc src/link_layer/bidiretional_node_b.c src/util/frame.c src/util/mq_utils.c -o bin/server -lrt
 
 echo "Compilando camada superior"
-gcc src/node_layer/app.c src/util/mq_utils.c -o bin/app -lrt -pthread
+gcc src/higher_layer/app.c src/util/mq_utils.c -o bin/app -lrt -pthread
